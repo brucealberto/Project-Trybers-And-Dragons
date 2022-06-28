8ad514bdeec7c011ffa4a3e05f3acb9316095511
Bruce Alberto <brucealberto2@gmail.com>
2022-06-27 22:23:26 -0300

    feat create Class Orc

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -2,8 +2,10 @@ import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf';
 import Halfling from './Halfling';
+import Orc from './Orc';
 
 export { Dwarf };
 export { Elf };
 export { Halfling };
+export { Orc };
 export default Race;
