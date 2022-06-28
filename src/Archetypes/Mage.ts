a059b5d6633c85c1f2c828f458116192847adc79
Bruce Alberto <brucealberto2@gmail.com>
2022-06-28 09:59:47 -0300

    feat create Interface Archetype

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,14 @@
+// import { EnergyType } from "../Energy";
+// import Archetype from "./Archetype";
+
+// export default class Mage extends Archetype {
+//   constructor(name:string, special:number, cost:number){
+//     super(name, special	,cost)
+//   }
+  
+  
+//   get energyType(): EnergyType {
+//     throw new Error("Method not implemented.");
+//   }
+  
+// }
