b999be96175618497eeed4941c80dc1895adae3b
Bruce Alberto <brucealberto2@gmail.com>
2022-06-28 11:03:52 -0300

    feat created extended Class Warrior

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -2,8 +2,10 @@ import Archetype from './Archetype';
 import Mage from './Mage';
 import Necromancer from './Necromancer';
 import Ranger from './Ranger';
+import Warrior from './Warrior';
 
 export { Mage };
 export { Necromancer };
 export { Ranger };
+export { Warrior };
 export default Archetype;
