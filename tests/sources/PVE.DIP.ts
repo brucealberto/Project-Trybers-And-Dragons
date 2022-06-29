40609aebbd451eabf3b040df5b4642db3c3e85f4
Bruce Alberto <89738047+brucealberto@users.noreply.github.com>
2022-06-29 12:07:10 -0300

    fix test

--- tests/sources/PVE.DIP.ts
+++ tests/sources/PVE.DIP.ts
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import { PVE } from '../../src/Battle';
-import Character from '../../src/Character';
-import Fighter, { SimpleFighter } from '../../src/Fighter';
-import Monster from '../../src/Monster';
-
-class F implements Fighter {
-  constructor(
-    public lifePoints = 10,
-    public strength = 10,
-    public defense = 10,
-  ) { }
-  levelUp(): void { }
-  special(enemy: SimpleFighter): void { }
-  attack(enemy: SimpleFighter): void { }
-  receiveDamage(amount: number): void { }
-}
-
-class S implements SimpleFighter {
-  constructor(
-    public lifePoints = 10,
-    public strength = 10,
-  ) { }
-  attack(enemy: SimpleFighter): void { }
-  receiveDamage(amount: number): void { }
-}
-
-const pve1 = new PVE(new Character(''), [new Monster()]);
-const pve2 = new PVE(new F(), [new S()]);
-=======
 import { PVE } from '../../src/Battle';
 import Character from '../../src/Character';
 import Fighter, { SimpleFighter } from '../../src/Fighter';
@@ -56,4 +26,3 @@ class S implements SimpleFighter {
 
 const pve1 = new PVE(new Character(''), [new Monster()]);
 const pve2 = new PVE(new F(), [new S()]);
->>>>>>> 3f434069382e9aed5b4448880b087260b658df63
